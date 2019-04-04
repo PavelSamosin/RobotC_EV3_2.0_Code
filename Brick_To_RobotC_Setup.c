c63a216278ea3197b8e49d1789d2f41b64c6b38f
PavelSamosin <48923458+PavelSamosin@users.noreply.github.com>
2019-04-04 15:34:39 -0400

    Add files via upload

---
+++ Brick_To_RobotC_Setup.c
@@ -0,0 +1,20 @@
+//Installation and Brick connection through bluetoth to robotC.
+//BY: Pavel Samosin
+//4/4/2019
+//LSA: ROBOTC_EV3_2.0
+//------------------------------------------------------------------------------------------------------------------------------
+//BEFORE STARTING TO CODE, FOLLOW THESE STEPS TO ENABLE THE BRICK TO ROBOTC!
+//
+//Step 1: Connect ev3 brick via bluetooth to the computer.
+//Step 2: When connected, on robotC, go to the window, Robot > LEGO Brick > Communication Link Setup
+//Step 3: Make sure that its searching 'BlueTooth', and find your brick there.
+//Step 4: Verify connection between brick and robotC by clicking 'Download to Robot' with this file
+//Step 5: If you get an error, click on 'Firmware Download', and retry Step 4
+//Step 6: If successful, then the connection is there and you can start programming your robot.
+//
+//----------------------------------CODE----------------------------------------------------------------------------------------
+
+task main()
+{
+//Just a function to test the uploading to the brick.
+}
